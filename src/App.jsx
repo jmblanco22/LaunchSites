3d72079c7c653aafd57c090c44ebbb83d9d03630
José Blanco <Jose Blanco>
2025-07-29 14:20:32 -0500

    add browserlink to react dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './components/MainPage';
